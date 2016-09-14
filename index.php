c5b8239560b26189de2ef9af9a1aa7eec76af0a1
Alexstraza <abdelouzzine@gmail.com>
2016-09-14 08:49:12 +0200

    Development started on homepage.

---
+++ index.php
@@ -0,0 +1,34 @@
+
+
+<?php
+
+include('views/header.html');
+
+
+$page = (empty($_GET['page']))?'' : $_GET['page'];
+
+switch($page) {
+    case 'projects';
+        include 'views/projects.html';
+        break;
+
+    case 'contact';
+        include 'views/contact.html ';
+        break;
+
+    case 'home';
+        include 'views/home.html ';
+        break;
+
+    default:
+        include 'views/home.html';
+
+}
+
+//include "views/footer.php";
+
+
+
+
+
+?>
